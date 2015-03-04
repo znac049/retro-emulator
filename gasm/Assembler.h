b1925379a9b3df62055c700581bcb2a8d1b429d8
Bob Green <bob@wookey.org.uk>
2015-03-04 15:51:25 +0000

    Starting on generic assembler

---
+++ gasm/Assembler.h
@@ -0,0 +1,8 @@
+#ifndef _ASSEMBLER_H_
+#define _ASSEMBLER_H_
+
+class Assembler 
+{
+};
+
+#endif
---
+++ gasm/gasm.h
@@ -0,0 +1,15 @@
+#ifndef _GASM_H_
+#define _GASM_H_
+
+#ifndef NULL
+# define NULL 0
+#endif
+
+#define MAXSTR 255
+
+typedef unsigned char byte;
+typedef unsigned short word;
+
+typedef unsigned int useconds_t;
+
+#endif
